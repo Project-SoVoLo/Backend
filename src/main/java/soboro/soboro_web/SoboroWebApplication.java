7ea113a518da4604bbd712f76c4b696874640816
차아현 <chaahyun@chaahyeon-ui-MacBookPro.local>
2025-04-02 00:22:18 +0900

    Initial commit

---
+++ src/main/java/soboro/soboro_web/SoboroWebApplication.java
@@ -0,0 +1,13 @@
+package soboro.soboro_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoboroWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoboroWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/soboro/soboro_web/SoboroWebApplicationTests.java
@@ -0,0 +1,13 @@
+package soboro.soboro_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoboroWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
